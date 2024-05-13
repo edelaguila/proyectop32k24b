65ef425d1935147de7b2b446a72ed8fed8baef6e
Kevin González Sosa <123187500+kgonzalez26@users.noreply.github.com>
2024-05-12 22:41:25 -0600

    Reportes Tipos de moneda

--- src/main/java/controlador/TiposDeMoneda.java
+++ src/main/java/controlador/TiposDeMoneda.java
@@ -12,13 +12,15 @@ public class TiposDeMoneda {
 
     private int id_moneda;
     private String nombre_moneda;
+    private String valor_moneda;
 
     public TiposDeMoneda() {
     }
 
-    public TiposDeMoneda(int id_moneda, String nombre_moneda) {
+    public TiposDeMoneda(int id_moneda, String nombre_moneda, String valor_moneda) {
         this.id_moneda = id_moneda;
         this.nombre_moneda = nombre_moneda;
+        this.valor_moneda = valor_moneda;
     }
 
     public int getIdMoneda() {
@@ -36,9 +38,17 @@ public class TiposDeMoneda {
     public void setNombreMoneda(String nombre_moneda) {
         this.nombre_moneda = nombre_moneda;
     }
+    
+    public String getValorMoneda() {
+        return valor_moneda;
+    }
+
+    public void setValorMoneda(String valor_moneda) {
+        this.valor_moneda = valor_moneda;
+    }
 
     @Override
     public String toString() {
-        return "TiposDeMoneda{" + "id_moneda=" + id_moneda + ", nombre_moneda=" + nombre_moneda + '}';
+        return "TiposDeMoneda{" + "id_moneda=" + id_moneda + ", nombre_moneda=" + nombre_moneda + ", nombre_moneda=" + valor_moneda +'}';
     }
 }
